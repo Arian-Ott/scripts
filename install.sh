4cad0d90f1724f5f9b4eec81109676f984377223
Arian <88984301+Arian-Ott@users.noreply.github.com>
2024-05-07 00:42:35 +0200

    Changed files

--- install.sh
+++ install.sh
@@ -4,10 +4,14 @@ apt install python3.12, python3.12-venv, python3.12-pip -y > /dev/null
 cd 
 mkdir .setup 
 cd .setup 
+apt install curl > /dev/null
+apt install git >/dev/null
+
+git clone https://github.com/Arian-Ott/scripts/
 python3.12 -m venv venv
 source venv/bin/activate 
 python3.12 -m pip install typer -y 
-python3.12 main.py 
+python3.12 scripts/aaa.py
+
 cd ..
-apt install curl > /dev/null
 rm -rf .setup
