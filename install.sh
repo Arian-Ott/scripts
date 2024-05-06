838405a6289fdce4996c6b3c415e5999623f1a8d
Arian <88984301+Arian-Ott@users.noreply.github.com>
2024-05-07 00:54:43 +0200

    CHANGED install file
    Signed-off-by: Arian <88984301+Arian-Ott@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #bin/bash
 apt update && apt upgrade -y > /dev/null
-apt install python3.12, python3.12-venv, python3.12-pip -y > /dev/null
-cd 
-mkdir .setup 
-cd .setup 
+apt-get install python3.11 -y && apt-get install  python3.11-venv -y && apt-get install  python3-pip -y > /dev/null
+cd
+mkdir ~/.setup
+cd ~/.setup
 apt install curl > /dev/null
 apt install git >/dev/null
 
 git clone https://github.com/Arian-Ott/scripts/
-python3.12 -m venv venv
-source venv/bin/activate 
-python3.12 -m pip install typer -y 
-python3.12 scripts/aaa.py
+python3 -m venv venv
+source venv/bin/activate
+python3 -m pip3 install typer -y
+python3 ~/.setup/scripts/main.py
 
 cd ..
-rm -rf .setup
+#rm -rf .setup
